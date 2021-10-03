d36e6c01021a66134404e29dad03050b7a524caa
Christophe <xt0ph@adiczion.net>
2021-10-03 20:35:53 +0200

    Release v14.0.3

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 # contains the full copyright notices and license terms.
 {
     'name': 'Chronopost Shipping',
-    'version': '14.0.2',
+    'version': '14.0.3',
     'author': 'Adiczion SARL',
     'category': 'Adiczion',
     'license': 'AGPL-3',

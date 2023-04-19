bb31d695ddd9e272861d66b322d07c671f60fd3b
remihb <remi@hobo.live>
2023-04-19 15:56:18 +0200

    [MIG] a4o_delivery_chronopost: Migration to 16.0

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 # contains the full copyright notices and license terms.
 {
     'name': 'Chronopost Shipping',
-    'version': '15.1.7',
+    'version': '16.0.1.0.0',
     'author': 'Adiczion SARL',
     'category': 'Adiczion',
     'license': 'AGPL-3',
